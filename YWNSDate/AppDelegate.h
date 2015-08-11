76218a54ee49369fb7847a094e066918e1e32e47
Yoni <yw_qisimao@163.com>
2015-08-11 14:12:25 +0800

    Initial Commit

---
+++ YWNSDate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YWNSDate
+//
+//  Created by aitaomama-ios on 15/8/11.
+//  Copyright (c) 2015年 Yoni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YWNSDate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YWNSDate
+//
+//  Created by aitaomama-ios on 15/8/11.
+//  Copyright (c) 2015年 Yoni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
